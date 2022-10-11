a2cde75ea5a3b13cbb38391d00c68750156d3f67
silvanoamaral <silvano.amaral9@gmail.com>
2022-10-10 22:01:07 -0300

    ✨ Add marvel api

--- src/App.js
+++ src/App.js
@@ -1,7 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+
+import logo from "./logo.svg";
+import "./App.css";
+
+import { config } from "./config";
+import { useHash } from "./hooks";
 
 function App() {
+  const [data, setData] = useState({});
+  const [loading, setLoading] = useState(false);
+
+  const [hash, timestamp] = useHash();
+  const limit = 20;
+  const currentOffset = 0;
+  const orderBy = "name";
+
+  useEffect(() => {
+    const url = `${config.marvel.url}/characters?limit=${limit}&ts=${timestamp}&offset=${currentOffset}&orderBy=${orderBy}&apikey=${config.marvel.publicKey}&hash=${hash}`;
+
+    const fetchData = async () => {
+      setLoading(true);
+
+      const response = await fetch(url);
+      const result = await response.json();
+
+      setData(result);
+      setLoading("fetched");
+    };
+
+    fetchData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  console.log(data);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +49,8 @@ function App() {
         >
           Learn React
         </a>
+
+        {loading && <p>carregando...</p>}
       </header>
     </div>
   );
---
+++ src/config/config.js
@@ -0,0 +1,10 @@
+const config = {
+  app: {},
+  marvel: {
+    publicKey: process.env.REACT_APP_PUBLICK_KEY,
+    privateKey: process.env.REACT_APP_PRIVATE_KEY,
+    url: process.env.REACT_APP_API_BASE_URI,
+  },
+};
+
+export { config };
